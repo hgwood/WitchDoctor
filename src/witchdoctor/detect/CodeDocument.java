c63f45cff661132df368ad58d3f0b1ae274d8e62
Hugo Wood <hx4.5@free.fr>
2012-05-24 03:41:18 -0700

    Some documentation.

--- src/witchdoctor/detect/CodeDocument.java
+++ src/witchdoctor/detect/CodeDocument.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface CodeDocument {
 	
 	/**
-	 * Returns the code of the document as an immutable list.
+	 * Returns the code of the document as an immutable list of lines.
 	 * @return
 	 */
 	List<String> getLines();
--- src/witchdoctor/detect/diff/code/CodeChunk.java
+++ src/witchdoctor/detect/diff/code/CodeChunk.java
@@ -10,6 +10,13 @@ import witchdoctor.detect.diff.Chunk;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Lists;
 
+/**
+ * A code fragment of zero or more lines 
+ * that belongs to a larger piece of code.
+ * Its lines can be iterated.
+ * @author Hugo Wood
+ *
+ */
 public class CodeChunk implements Chunk, Iterable<String> {
 	
 	private final int position;
@@ -20,15 +27,29 @@ public class CodeChunk implements Chunk, Iterable<String> {
 		this.lines = FluentIterable.from(lines);
 	}
 	
+	/**
+	 * Returns a specific line of the chunk.
+	 * @param index
+	 * @return
+	 * @throws IndexOutOfBoundsException if index is below zero or if index
+	 * is greater than the size of the chunk minus one.
+	 */
 	public String getLine(int index) {
 		return lines.get(index);
 	}
 
+	/**
+	 * Returns the position of this chunk relative to the code it originates
+	 * from (i.e. the line number minus one).
+	 */
 	@Override
 	public int getPosition() {
 		return position;
 	}
 
+	/**
+	 * Returns the size of this chunk in number of lines.
+	 */
 	@Override
 	public int getSize() {
 		return lines.size();
@@ -39,6 +60,10 @@ public class CodeChunk implements Chunk, Iterable<String> {
 		return lines.iterator();
 	}
 	
+	/**
+	 * Splits this chunks into multiple chunks spanning only one line.
+	 * @return
+	 */
 	public Iterable<CodeChunk> explodeLines() {
 		Collection<CodeChunk> subChunks = Lists.newLinkedList();
 		int currentPosition = position;
