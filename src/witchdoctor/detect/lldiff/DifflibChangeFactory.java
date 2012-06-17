f3ec7c0a2031742e5e4e11d99e764d6420966e64
Hugo Wood <hx4.5@free.fr>
2012-06-16 21:43:10 -0700

    Moved functionality of DeltaFactory to DifflibChangeFactory

--- src/witchdoctor/detect/lldiff/DifflibChangeFactory.java
+++ src/witchdoctor/detect/lldiff/DifflibChangeFactory.java
@@ -1,10 +1,17 @@
 package witchdoctor.detect.lldiff;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 import witchdoctor.WitchDoctorException;
 import witchdoctor.detect.changes.IChangeFactory;
 import witchdoctor.detect.changes.IMacroChange;
 import difflib.Chunk;
+import difflib.DeleteDelta;
 import difflib.Delta;
+import difflib.InsertDelta;
 import difflib.Delta.TYPE;
 
 /**
@@ -23,6 +30,36 @@ public class DifflibChangeFactory {
 	public DifflibChangeFactory(IChangeFactory changeFactory) {
 		super();
 		this.changeFactory = changeFactory;
+	}
+	
+	/**
+	 * Transforms deltas of type CHANGE into two deltas: one INSERT and one
+	 * DELETE. Other deltas are left unchanged.
+	 * @param deltas a collection of deltas of any type
+	 * @return a collection of deltas of type INSERT or DELETE
+	 */
+	public Iterable<Delta> cutUpdates(Iterable<Delta> deltas) {
+		List<Delta> cuts = Lists.newLinkedList();
+		for (Delta delta : deltas) {
+			switch (delta.getType()) {
+			case CHANGE:
+				Chunk emptyOriginal = new Chunk(
+					delta.getOriginal().getPosition(), 
+					Collections.EMPTY_LIST);
+				Delta ins = new InsertDelta(emptyOriginal, delta.getRevised());
+				cuts.add(ins);
+				Chunk emptyRevised = new Chunk(
+					delta.getRevised().getPosition(), 
+					Collections.EMPTY_LIST);
+				Delta del = new DeleteDelta(delta.getOriginal(), emptyRevised);
+				cuts.add(del);
+				break;
+			default:
+				cuts.add(delta);
+				break;
+			}
+		}
+		return cuts;
 	}
 
 	/**
@@ -46,6 +83,26 @@ public class DifflibChangeFactory {
 			isdeletion, 
 			content.getPosition(), 
 			(Iterable<Object>)content.getLines());
+	}
+	
+	/**
+	 * Same as the other version of create, but for a collection of deltas.
+	 * The collection can contain deltas of type CHANGE. Those will 
+	 * be cut using {@link cutUpdates}.
+	 * @param deltas
+	 * @return
+	 */
+	public Iterable<IMacroChange<Object>> create(Iterable<Delta> deltas) {
+		List<IMacroChange<Object>> changes = Lists.newLinkedList();
+		Iterable<Delta> cuts = cutUpdates(deltas);
+		for (Delta delta : cuts) {
+			try {
+				changes.add(create(delta));
+			} catch (WitchDoctorException e) {
+				assert false;
+			}
+		}
+		return changes;
 	}
 
 }
