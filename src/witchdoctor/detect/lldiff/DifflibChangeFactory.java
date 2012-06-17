6948c037034543baf8a8d09e0581224a222b19c5
Hugo Wood <hx4.5@free.fr>
2012-06-16 21:08:23 -0700

    IChangeFactory dependence of DifflibFactory is now passed through the constructor.

--- src/witchdoctor/detect/lldiff/DifflibChangeFactory.java
+++ src/witchdoctor/detect/lldiff/DifflibChangeFactory.java
@@ -8,13 +8,23 @@ import difflib.Delta;
 import difflib.Delta.TYPE;
 
 /**
- * Creates WitchDoctor's IChange objects from Delta object (from the diffutils
- * library).
+ * Creates WitchDoctor's IChange objects from the diffutils library's objects.
  * @author Hugo Wood
  *
  */
 public class DifflibChangeFactory {
 	
+	private final IChangeFactory changeFactory; 
+	
+	/**
+	 * 
+	 * @param changeFactory will build IChange object from diffutils' objects info
+	 */
+	public DifflibChangeFactory(IChangeFactory changeFactory) {
+		super();
+		this.changeFactory = changeFactory;
+	}
+
 	/**
 	 * Adapts Delta object from the diffutils library to WitchDoctor's IChange
 	 * objects. Since diffutils' Deltas can contain several changes, a 
@@ -23,17 +33,16 @@ public class DifflibChangeFactory {
 	 * Deltas representing an update (type CHANGE in diffutils) cannot be 
 	 * processed by this method.
 	 * @param delta
-	 * @param factory will build IChange object from the Delta object info
 	 * @return
 	 * @throws WitchDoctorException if delta is of type CHANGE
 	 */
 	@SuppressWarnings("unchecked")
-	public IMacroChange<Object> create(Delta delta, IChangeFactory factory) 
+	public IMacroChange<Object> create(Delta delta) 
 	throws WitchDoctorException {
 		if (delta.getType() == TYPE.CHANGE) { throw new WitchDoctorException(); }
 		boolean isdeletion = delta.getType() == TYPE.DELETE;
 		Chunk content = isdeletion ? delta.getOriginal() : delta.getRevised();
-		return factory.createMacro(
+		return changeFactory.createMacro(
 			isdeletion, 
 			content.getPosition(), 
 			(Iterable<Object>)content.getLines());
