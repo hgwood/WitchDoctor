ff757f23b7b52f416918f8081037cbe5c15ae70b
Hugo Wood <hx4.5@free.fr>
2012-06-17 00:24:13 -0700

    createMacro and split in ChangeFactory use create to create Changes, so it is easier to subclass.

--- src/witchdoctor/detect/changes/ChangeFactory.java
+++ src/witchdoctor/detect/changes/ChangeFactory.java
@@ -13,7 +13,7 @@ public class ChangeFactory implements IChangeFactory {
 	
 	@Override
 	public <T> IMacroChange<T> createMacro(boolean isdeletion, int position, Iterable<T> content) {
-		IChange decorated = new Change(isdeletion, position, null);
+		IChange decorated = create(isdeletion, position, null);
 		return new MacroChange<T>(decorated, content);
 	}
 	
@@ -22,7 +22,7 @@ public class ChangeFactory implements IChangeFactory {
 		List<IChange> changes = Lists.newLinkedList();
 		int currentPosition = 0;
 		for (T item : macroChange) {
-			IChange change = new Change(
+			IChange change = create(
 				macroChange.isDeletion(), 
 				currentPosition++, 
 				item);
